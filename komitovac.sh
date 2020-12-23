c639e85140dbc4715f667a1970a63b186dc6d57d
Jakub Ambroz <ambrojak@fel.cvut.cz>
2020-12-23 23:33:26 +0100

    auto-commit added

---
+++ komitovac.sh
@@ -0,0 +1,10 @@
+eval $(ssh-agent -s)
+ssh-add /home/ambry/.ssh/id_rsa_ambrojak
+ssh-add /home/ambry/.ssh/id_rsa_ambry
+git add .
+git commit
+git push
+
+#personal - My WSL is kinda broken
+#in case of WSL internet issues, use:
+#sudo bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
