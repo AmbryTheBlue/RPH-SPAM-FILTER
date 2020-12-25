a5b8b5ea1f87d3dfe73333767dbb7eb05c615106
Jakub Ambroz <ambrojak@fel.cvut.cz>
2020-12-25 23:57:53 +0100

    fix file organization

--- myfilter.py
+++ myfilter.py
@@ -5,8 +5,10 @@
 #https://github.com/AmbryTheBlue/RPH-SPAM-FILTER
 
  
- import basefilter
- #this also imports corpus, utils a os (snad xD)
- import trainingcorpus #zatim k nicemu
- from utils_email import array_from_mail
+import basefilter #this also imports corpus, utils a os (snad xD)
+import trainingcorpus #zatim k nicemu
+import utils_cleaner as cleaner
+import utils_analyzer as analyzer
+
+from utils_email import array_from_mail
  
