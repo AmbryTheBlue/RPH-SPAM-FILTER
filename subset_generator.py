f7c3e3f6e5c5d5ed4f2a75aff198a7a9e64ff052
Jakub Ambroz <ambrojak@fel.cvut.cz>
2020-12-26 16:23:32 +0100

    experiments with counters Co-authored-by: Katerina Kucerova <kucerka7@fel.cvut.cz>

--- subset_generator.py
+++ subset_generator.py
@@ -2,35 +2,39 @@ import utils
 import os
 import random
 
-path = "spam-data-12-s75-h25"
-dictionary1 = utils.read_classification_from_file(path+"/1/!truth.txt")
-dictionary2 = utils.read_classification_from_file(path+"/2/!truth.txt")
-print(dictionary1)
-print(dictionary2)
-dict_train = {}
-dict_test = {}
 
-save_path = "my_data/"
-save_path += "sub1"
-print("Removing '" + save_path + "' if exists")
-os.system("rm -rf " + save_path)
-print("Creating appropriate directories.")
-os.system("mkdir " + save_path)
-os.system("mkdir " + save_path+"/train/")
-os.system("mkdir " + save_path+"/test/")
-percantage_used = 30  # percantage portion that is used for training
-print("Saving file into data set:")
-for name, status in dictionary1.items():
-    if (random.randint(1, 100) <= 50):
-        dict_train[name] = status
-        print("Train: " + name + " : " + status)
-        os.system("cp " + path + "/1/"+name + " " + save_path+"/train/"+name)
-    else:
-        dict_test[name] = status
-        print("Test: " + name + " : " + status)
-        os.system("cp " + path + "/1/"+name + " " + save_path+"/test/"+name)
+def random_subset(number, name):
+    path = "spam-data-12-s75-h25"
+    dict_train = {}
+    dict_test = {}
+    save_path = "my_data/"
+    dictionary = utils.read_classification_from_file(
+        path+"/"+ str(number) +"/!truth.txt")
+    save_path += name
+    print("Removing '" + save_path + "' if exists")
+    os.system("rm -rf " + save_path)
+    print("Creating appropriate directories.")
+    os.system("mkdir " + save_path)
+    os.system("mkdir " + save_path+"/train/")
+    os.system("mkdir " + save_path+"/test/")
+    percantage_used = 30  # percantage portion that is used for training
+    print("Saving file into data set:")
+    for name, status in dictionary.items():
+        if (random.randint(1, 100) <= 50):
+            dict_train[name] = status
+            print("Train: " + name + " : " + status)
+            os.system("cp " + path + "/" + str(number) + "/" +
+                      name + " " + save_path+"/train/"+name)
+        else:
+            dict_test[name] = status
+            print("Test: " + name + " : " + status)
+            os.system("cp " + path + "/" + str(number) + "/" +
+                      name + " " + save_path+"/test/"+name)
+    utils.write_dict_to_file(save_path+"/train/!truth.txt", dict_train)
+    utils.write_dict_to_file(save_path+"/test/!truth.txt", dict_test)
+    print("Succcess!\n Subsets created:\n" +
+          save_path+"/train/\n"+save_path+"/test/")
 
-utils.write_dict_to_file(save_path+"/train/!truth.txt", dict_train)
-utils.write_dict_to_file(save_path+"/test/!truth.txt", dict_test)
-print("Succcess!\n Subsets created:\n" +
-      save_path+"/train/\n"+save_path+"/test/")
+
+if __name__ == "__main__":
+    random_subset(1, "sub2")
--- utils_analyzer.py
+++ utils_analyzer.py
@@ -2,13 +2,37 @@ from collections import Counter
 
 
 def counter_from_array(array):
-    # useless function
+    # useless function, remains for compatibility 
     c = Counter(array)
     return c
 
+def counter_difference(c_spam, c_ham, limit):
+    #NOT WORKING, remains for compatibility 
+    #replaced by c_spam - c_ham
+    spam_most = c_spam.most_common(limit)
+    ham_most = c_ham.most_common(limit)
+    i = 0
+    print(len(spam_most))
+    print("-------------------------")
+    #print(spam_most)
+    print("-------------------------")
+    #print(ham_most)
+    print("-------------------------")
+    while (i < len(spam_most)):
+        intersection = False
+        for el in ham_most:
+            #print(str(el[0]) + " == " + str(spam_most[i][0]))
+            if(el[0] == spam_most[i][0]):
+                intersection = True
+                break
+        if(intersection):
+            print(len(spam_most))
+            spam_most.pop(i)
+            print(len(spam_most))
+        i += 1
+    print(len(spam_most))
+    return spam_most
 
-def counter_difference(c1, c2):
-    pass
 
 def char_count(char, txt):
     count = 0
@@ -48,24 +72,44 @@ if __name__ == "__main__":
             if char == '%':
                 count += 1
         return count
+    c1 = {}
     with open("spam-data-12-s75-h25/1/0001.bfc8d64d12b325ff385cca8d07b84288") as soubor:
         content = array_from_mail(soubor.read())[-1][1]
         # print(content)
         #print("\n -------------------------------------- \n")
         non_html = remove_html_tags(content)
-        print(non_html)
+        # print(non_html)
         spaceless = remove_white_space(non_html)
-        print(spaceless)
-        print("Amount of exclamation marks('!'): " +
-              str(count_exclamation(spaceless)))
-        print("Amount of percentages('%'): " +
-              str(count_percantage(spaceless)))
+        # print(spaceless)
+        #print("Amount of exclamation marks('!'): " + str(count_exclamation(spaceless)))
+        #print("Amount of percentages('%'): " + str(count_percantage(spaceless)))
         clean = remove_punctutation(spaceless)
-        print(clean)
+        # print(clean)
         array = word_arr_from_string(clean)
-        print(array)
-        print("Count capslocked: " + str(count_capslocked_words(array)))
+        # print(array)
+        #print("Count capslocked: " + str(count_capslocked_words(array)))
         array2 = sync_capitalization_of_arr(array)
-        print(array2)
-        c = counter_from_array(array2)
-        print(c)
+        # print(array2)
+        c1 = counter_from_array(array2)
+        print("---------------------------")
+        print(c1)
+        print("---------------------------")
+    c2 = {}
+    with open("spam-data-12-s75-h25/1/00029.cc0c62b49c1df0ad08ae49a7e1904531") as soubor:
+        content = array_from_mail(soubor.read())[-1][1]
+        non_html = remove_html_tags(content)
+        spaceless = remove_white_space(non_html)
+        clean = remove_punctutation(spaceless)
+        array = word_arr_from_string(clean)
+        array2 = sync_capitalization_of_arr(array)
+        c2 = counter_from_array(array2)
+        print(c2)
+        print("---------------------------")
+    c_diff = counter_difference(c1, c2, 50)
+    c_diff2 = c1-c2
+    print(c_diff)
+    print("---------------------------")
+    print(c_diff2)
+    c_diff3 = c2-c1
+    print("---------------------------")
+    print(c_diff3)
