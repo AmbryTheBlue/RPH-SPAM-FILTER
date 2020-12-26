e76efdff1b8549fca8e6909046e2c885440874ea
Jakub Ambroz <ambrojak@fel.cvut.cz>
2020-12-26 12:24:29 +0100

    subset generator fixes readibility and reusibility

--- subset_generator.py
+++ subset_generator.py
@@ -10,16 +10,26 @@ print(dictionary2)
 dict_train = {}
 dict_test = {}
 
-save_path = "my_data/sub1"
-percantage_used = 50
+save_path = "my_data/"
+save_path += "sub1"
+print("Removing '" + save_path + "' if exists")
+os.system("rm -rf " + save_path)
+print("Creating appropriate directories.")
+os.system("mkdir " + save_path)
+os.system("mkdir " + save_path+"/train/")
+os.system("mkdir " + save_path+"/test/")
+percantage_used = 30 #percantage portion that is used for training
 for name, status in dictionary1.items():
-    print(name + " : " + status)
-    if (random.randint(1,100)>=50):
+    print("Saving file into data set:")
+    if (random.randint(1,100)<=50):
         dict_train[name] = status
+        print("Train: " + name + " : " + status)
         os.system("cp " + path + "/1/"+name + " " + save_path+"/train/"+name)
     else:
         dict_test[name] = status
+        print("Test: " + name + " : " + status)
         os.system("cp " + path + "/1/"+name + " " + save_path+"/test/"+name)
 
 utils.write_dict_to_file(save_path+"/train/!truth.txt", dict_train)
 utils.write_dict_to_file(save_path+"/test/!truth.txt", dict_test)
+print("Succcess!\n Subsets created:\n" +save_path+"/train/\n"+save_path+"/test/")
